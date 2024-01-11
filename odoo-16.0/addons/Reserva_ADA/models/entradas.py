ae660f494550d1e67486481763b14457ea4ac66a
Diego <Conkergox98gmail.com>
2024-01-11 10:07:32 +0100

    Vista de entrada acabada y retoque de la entidad

--- odoo-16.0/addons/Reserva_ADA/models/entradas.py
+++ odoo-16.0/addons/Reserva_ADA/models/entradas.py
@@ -1,29 +1,50 @@
-# -*- coding: utf-8 -*-
-
 from odoo import models, fields, api, exceptions
 
+
+
+
 class entradas(models.Model):
-    _name = 'grupo3c.entradas'
+   _name = 'grupo3c.entradas'
+   #Precio de la entrada
+   precio = fields.Float(required= True, string="Precio");
+   fecha_entrada = fields.Date(required=True, string="Fecha")
 
-    precio = fields.Monetary(required = True, string="Precio")
-    fecha_entrada = fields.Date(required = True, string ="Fecha")
+
+   client_id = fields.Many2one('res.users', string="Cliente")
+   admin_id = fields.Many2one('res.users', string="Admin")
+   zona_id = fields.Many2many('grupo3c.zonas', string="Código de Zona")
 
 
-    client_id = fields.Many2one('res.users', string="Cliente")
-    admin_id = fields.Many2one('res.users', string="Admin")
-    zona_id = fields.ManyMany("grupo3c.zona", "zonaId", string="codigo_zona")
+   @api.onchange('fecha_entrada')
+   def _onchange_fecha_entrada(self):
+       if self.fecha_entrada and self.fecha_entrada < fields.Date.today():
+           return {
+               'warning': {
+                   'title': "Advertencia",
+                   'message': "La fecha no puede ser inferior a la actual."
+               }
+           }
+   @api.constrains('fecha_entrada')
+   def _check_fecha_entrada(self):
+       for entradas in self:
+           if fields.Date.from_string(entradas.fecha_entrada) < fields.Date.from_string(fields.Date.today()):
+               raise exceptions.ValidationError("La fecha no puede ser inferior a la actual")
 
-    @api.constrains('fecha_entrada')
-    def _validate_date(self):
-        for entradas in self:
-            if fields.Date.from_string(entradas.fecha_entrada) < fields.Date.from_string(fields.Date.today()):
-                raise exceptions.ValidationError("La fecha no puede ser inferior a la actual")
 
-    @api.constrains('precio')
-    def _validate_price(self):
-        for entradas in self:
-            if entradas.precio.isalpha():
-                raise exceptions.ValidationError("No puedes introducir letras en el precio")
+   @api.onchange('precio')
+   def _onchange_precio(self):
+       if not isinstance(self.precio, (int, float)):
+           return {
+               'warning': {
+                   'title': "Advertencia",
+                   'message': "El precio debe ser un valor numérico."
+               }
+           }
+   @api.constrains('precio')
+   def _check_precio(self):
+       for entradas in self:
+           if entradas.precio.isalpha():
+               raise exceptions.ValidationError("No puedes introducir letras en el precio")
 
 
 
